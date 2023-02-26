946e97ed675df6ac71158fd1ee59dad82f28f022
Valentine <35977727+valentinegb@users.noreply.github.com>
2023-02-26 11:21:38 -0800

    Make `extract` command extract directories and symbolic links

--- src/app/extract.rs
+++ src/app/extract.rs
@@ -13,6 +13,14 @@ pub fn extract(args: ExtractArgs, read_unpacked: bool) -> Result<()> {
 		None
 	};
 	let reader = AsarReader::new(&file, asar_path).wrap_err("failed to read archive")?;
+	for path in reader.directories().keys() {
+		let out_path = args.destination.join(path);
+		if !out_path.starts_with(&args.destination) {
+			panic!("asar archive attempted to escape destination");
+		}
+		fs::create_dir(&out_path)
+			.wrap_err_with(|| format!("failed to write directory {}", out_path.display()))?;
+	}
 	for (path, file) in reader.files() {
 		let out_path = args.destination.join(path);
 		if !out_path.starts_with(&args.destination) {
@@ -20,6 +28,23 @@ pub fn extract(args: ExtractArgs, read_unpacked: bool) -> Result<()> {
 		}
 		fs::write(&out_path, file.data())
 			.wrap_err_with(|| format!("failed to write file {}", out_path.display()))?;
+	}
+	for (path, link) in reader.symlinks() {
+		let out_path = args.destination.join(path);
+		let out_link = args.destination.join(link);
+		if !out_path.starts_with(&args.destination) || !out_link.starts_with(&args.destination) {
+			panic!("asar archive attempted to escape destination");
+		}
+		#[cfg(all(unix))]
+		{
+			std::os::unix::fs::symlink(out_link, &out_path)
+				.wrap_err_with(|| format!("failed to write symbolic link {}", out_path.display()))?;
+		}
+		#[cfg(all(windows))]
+		{
+			std::os::windows::fs::symlink_file(out_link, &out_path)
+				.wrap_err_with(|| format!("failed to write symbolic link {}", out_path.display()))?;
+		}
 	}
 
 	Ok(())
